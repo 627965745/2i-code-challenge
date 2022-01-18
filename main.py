9fc1c63c22a9a3dd3b164aebbce43fed7fc95385
Jiaming Liu <forpad0520@gmail.com>
2022-01-18 16:44:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # 2i code challenge, assume all inputs are positive integers
+# A Python code that iterates in multiples of A until X, then in multiples of A + 1 until 2X, then multiples of A + 2 until 3X.
 def main():
   A = int(input("Enter value A: "))
   X = int(input("Enter value X: "))
   for a in range (A, X+1, A):
       print(a)
-  for a in range (A, 2*X, A+1):
+  for a in range (A+1, 2*X, A+1):
       print(a)
-  for a in range (A, 3*X, A+2):
+  for a in range (A+2, 3*X, A+2):
       print(a)
